d8bfe2f8732a4581af68d7c3e9cf714399f953cd
Ivan Jimenez <ivan_djimenez@soy.sena.edu.co>
2024-01-16 20:35:40 -0500

    Hice una Calculadora IMC

---
+++ CalculadoraIMC.java
@@ -0,0 +1,29 @@
+package Clase1;
+
+import java.util.Scanner;
+
+public class CalculadoraIMC {
+    public static void main (String [] args) {
+        System.out.println("Por ingrese su peso en Kilogramos: ");
+        Scanner cargaDeDatos = new Scanner(System.in);
+        double peso = cargaDeDatos.nextDouble();
+        System.out.println("Por favor ingrese su altura en Metros: ");
+        double altura = cargaDeDatos.nextDouble();
+
+        double indiceMasaCorporal = peso / (altura * altura);
+        System.out.println("Tu indice de masa corporal es: "+indiceMasaCorporal);
+
+        if (indiceMasaCorporal >= 30) {
+            System.out.println("Estas en estado de Obesidad");
+        }
+        if (indiceMasaCorporal >= 25 && indiceMasaCorporal < 30) {
+            System.out.println("Estas en estado de sobrepeso");
+        }
+        if (indiceMasaCorporal >= 18.5 && indiceMasaCorporal < 25) {
+            System.out.println("Estas en estado de peso normal");
+        }
+        if ( indiceMasaCorporal < 18.6) {
+            System.out.println("Estas en un peso menor al normal");
+        }
+    }
+}
